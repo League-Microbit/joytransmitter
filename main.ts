df0ebbc5f3077473bf50a4e2e9193f53144e4569
The LEAGUE of Amazing Programmers <it@jointheleague.org>
2024-08-09 09:58:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,34 +9,16 @@ music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.Pl
 radio.setGroup(1)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            . # # . .
-            # . . # .
-            # # # # .
-            # . . # .
-            # . . # .
-            `)
         radio.sendValue("b", 0)
     } else if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            # # # . .
-            # . . # .
-            # # # . .
-            # . . # .
-            # # # # .
-            `)
         radio.sendValue("b", 1)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
-        basic.showArrow(ArrowNames.West)
         radio.sendValue("b", 2)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
-        basic.showArrow(ArrowNames.North)
         radio.sendValue("b", 3)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
-        basic.showArrow(ArrowNames.East)
         radio.sendValue("b", 4)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
-        basic.showArrow(ArrowNames.South)
         radio.sendValue("b", 5)
     } else {
         basic.clearScreen()
@@ -47,5 +29,6 @@ basic.forever(function () {
         radio.sendValue("x", x)
         radio.sendValue("y", y)
         led.plot(px, py)
+        serial.writeValue("x", x)
     }
 })
